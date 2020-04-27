a9f27c12b4272954f651fb865230ae3c132c9760
mao <maoyuan@gmail.com>
2020-04-27 18:12:37 -0400

    manually fixed routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,42 +3,34 @@ import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 import {AppComponent} from './app.component';
-import {HeadlinesComponent} from './start-page/headlines/headlines.component';
 import {TestComp1Component} from './test-comp1/test-comp1.component';
 import {TestCardsComponent} from './test-cards/test-cards.component';
 import {TestCardholderComponent} from './test-cardholder/test-cardholder.component';
-import {HomepageComponent} from './start-page/homepage/homepage.component';
 import {SearchnewsComponent} from './searchnews/searchnews.component';
-import {QuotecardComponent} from './quote-trade/quotecard/quotecard.component';
-import {QuotepageComponent} from './quote-trade/quotepage/quotepage.component';
-import {TradeorderComponent} from './quote-trade/tradeorder/tradeorder.component';
 import {FeedbackComponent} from './feedback/feedback.component';
-import {StockindexComponent} from './start-page/stockindex/stockindex.component';
 import {QuoteTradeModule} from './quote-trade/quote-trade.module';
 import {StartPageModule} from './start-page/start-page.module';
+import { AppRoutingModule } from './app-routing.module';
+import {RouterModule} from '@angular/router';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    // HeadlinesComponent,
     TestComp1Component,
     TestCardsComponent,
     TestCardholderComponent,
-    // HomepageComponent,
     SearchnewsComponent,
-    // QuotecardComponent,
-    // QuotepageComponent,
-    // TradeorderComponent,
     FeedbackComponent,
-    // StockindexComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
     QuoteTradeModule,
-    StartPageModule
+    StartPageModule,
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/quote-trade/quote-trade-routing.module.ts
+++ src/app/quote-trade/quote-trade-routing.module.ts
@@ -1,11 +1,15 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {TradeorderComponent} from './tradeorder/tradeorder.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'trade', component: TradeorderComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class QuoteTradeRoutingModule { }
+export class QuoteTradeRoutingModule {
+}
