ff1d6032d8c99cf8a645092b2676ae92fb89fc99
mao <maoyuan@gmail.com>
2020-04-27 17:02:02 -0400

    created quote-trade-routing.module and start-page-routing.module

---
+++ src/app/quote-trade/quote-trade.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { QuoteTradeRoutingModule } from './quote-trade-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    QuoteTradeRoutingModule,
+  ]
+})
+export class QuoteTradeModule { }
