fb474804a70a7282cd85a43ca55fa7c82aef52f0
mao <maoyuan@gmail.com>
2020-04-27 20:00:23 -0400

    quote-and-trade-page

---
+++ src/app/quote-trade/quote-and-trade-page/quote-and-trade-page.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { QuoteAndTradePageComponent } from './quote-and-trade-page.component';
+
+describe('QuoteAndTradePageComponent', () => {
+  let component: QuoteAndTradePageComponent;
+  let fixture: ComponentFixture<QuoteAndTradePageComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ QuoteAndTradePageComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(QuoteAndTradePageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/quote-trade/quote-and-trade-page/quote-and-trade-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quote-and-trade-page',
+  templateUrl: './quote-and-trade-page.component.html',
+  styleUrls: ['./quote-and-trade-page.component.css']
+})
+export class QuoteAndTradePageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
