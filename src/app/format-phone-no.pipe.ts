24ba5d5ab92a4bd071a7d65cae29e237775ebaea
mao <maoyuan@gmail.com>
2020-05-04 15:30:21 -0400

    fixes phone number formatter pipe

--- src/app/format-phone-no.pipe.ts
+++ src/app/format-phone-no.pipe.ts
@@ -8,9 +8,9 @@ export class FormatPhoneNoPipe implements PipeTransform {
   transform(value: unknown, ...args: unknown[]): unknown {
     if (value !== null) {
       const s: string = String(value);
-      const pre: string = s.substring(0, 3);
-      const mid: string = s.substring(3, 6);
-      const end: string = s.substring(6, s.length);
+      const pre: string = s.substring(0, 4);
+      const mid: string = s.substring(4, 7);
+      const end: string = s.substring(7, s.length);
       return pre + '-' + mid + '-' + end;
     } else {
       return null;
