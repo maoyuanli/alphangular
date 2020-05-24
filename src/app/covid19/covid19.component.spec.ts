bf7938bdf847d1c50c2c03089574edd78bb90ad7
mao <maoyuan@gmail.com>
2020-05-24 17:03:46 -0400

    covi19 component

---
+++ src/app/covid19/covid19.component.spec.ts
@@ -0,0 +1,21 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Covid19Component } from './covid19.component';
+
+describe('Covid19Component', () => {
+  let component: Covid19Component;
+  let fixture: ComponentFixture<Covid19Component>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ Covid19Component ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(Covid19Component);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+});
---
+++ src/app/covid19/covid19.component.ts
@@ -0,0 +1,30 @@
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Covid19Service, Covid19Stats} from '../services/covid19.service';
+
+@Component({
+  selector: 'app-covid19',
+  templateUrl: './covid19.component.html',
+  styleUrls: ['./covid19.component.css']
+})
+export class Covid19Component implements OnInit {
+
+  currentStats: Covid19Stats = null;
+  toolTip = '';
+
+  constructor(private covid19Service: Covid19Service) {
+  }
+
+  ngOnInit(): void {
+    this.fetchCovid19Stats();
+  }
+
+  fetchCovid19Stats() {
+    this.covid19Service.fetchCovid19Stats().subscribe(statsArray => {
+      const latest: Covid19Stats = statsArray[statsArray.length - 1];
+      if (latest.Confirmed !== null && latest.Confirmed !== 0) {
+        this.currentStats = latest;
+        this.toolTip = `${latest.Active} Active; ${latest.Recovered} Recovered; ${latest.Deaths} Deaths`;
+      }
+    });
+  }
+}
