27ea5b30929417ad118973c43b7798701e67bc14
mao <maoyuan@gmail.com>
2020-07-02 12:25:26 -0400

    fixes start page stock index benchmark logic

--- src/app/start-page/stockindex/stockindex.component.ts
+++ src/app/start-page/stockindex/stockindex.component.ts
@@ -37,19 +37,19 @@ export class StockindexComponent implements OnInit {
           startIndex++;
         }
         const latest = data[startIndex];
-        let benchmark;
+        let benchmark = startIndex + 1;
         for (let i = startIndex + 1; i < data.length; i++) {
           if (this.avgSentScore >= 0 && data[i][1] < latest[1] || this.avgSentScore < 0 && data[i][1] > latest[1]) {
-            benchmark = data[i];
+            benchmark = i;
             break;
           }
         }
         this.indexData = {
           latest_date: latest[0],
           latest_val: latest[1],
-          benchmark_date: benchmark[0],
-          benchmark_val: benchmark[1],
-          change: latest[1] / benchmark[1] - 1
+          benchmark_date: data[benchmark][0],
+          benchmark_val: data[benchmark][1],
+          change: latest[1] / data[benchmark][1] - 1
         }
         ;
       }
